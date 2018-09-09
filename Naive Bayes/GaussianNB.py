9d4a424385ba2eb520b42ffc55a4a96f20fa4285
Vishal <vishal114186@gmail.com>
2018-09-09 23:28:20 +0530

    Naive Bayes Classifier

---
+++ Naive Bayes/GaussianNB.py
@@ -0,0 +1,17 @@
+"""
+Created on Sun Sep  9 20:39:19 2018
+@author: Vishal V
+
+"""
+#Using the iris dataset from sklearn
+from sklearn import datasets
+#Using a Gaussian Naive Bayes classifier
+from sklearn.naive_bayes import GaussianNB
+iris = datasets.load_iris()
+clf = GaussianNB()
+X = iris.data
+Y = iris.target
+pred = clf.fit(X, Y).predict(X)
+size = X.shape[0]
+value = (Y != pred).sum()
+print(f'The number of mislabeled points from {size} are: {value}')
