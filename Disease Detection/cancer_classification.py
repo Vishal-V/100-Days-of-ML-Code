b664d406e624f61b1342fc43f35843eb03177c38
Vishal-V <vishal114186@gmail.com>
2018-12-15 18:30:09 +0530

    GridSearch for model improvement

--- Disease Detection/cancer_classification.py
+++ Disease Detection/cancer_classification.py
@@ -63,7 +63,7 @@ df_data.tail()
 
 # Exploratory data analysis
 
-sns.pairplot(df_data, vars=['mean radius', 'mean texture', 'mean area'], hue='target')
+sns.pairplot(df_data, vars=['mean radius', 'mean texture', 'mean area', 'worst area'], hue='target')
 
 
 # In[11]:
@@ -93,10 +93,10 @@ sns.heatmap(df_data.corr(), annot=True)
 
 
 
-# In[14]:
+# In[15]:
 
 
-# Spit the training sets
+# Split the training sets
 
 from sklearn.model_selection import train_test_split
 
@@ -104,13 +104,27 @@ X = df_data.drop(['target'], axis=1)
 Y = df_data['target']
 
 
-# In[15]:
+# In[16]:
 
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
 
 
-# In[16]:
+# In[17]:
+
+
+# Normalization
+
+x_min = X_train.min()
+range_x = (X_train - x_min).max()
+X_train_scaled = (X_train - x_min)/range_x
+
+test_min = X_test.min()
+range_test = (X_test - test_min).max()
+X_test_scaled = (X_test - test_min)/range_test
+
+
+# In[18]:
 
 
 from sklearn.svm import SVC
@@ -119,8 +133,78 @@ from time import time
 
 model = SVC()
 t = time()
-model.fit(X_train, Y_train)
+model.fit(X_train_scaled, Y_train)
 print(f'Time taken = {time() - t} ms')
+
+
+# In[19]:
+
+
+pred = model.predict(X_test_scaled)
+acc = accuracy_score(pred, Y_test)
+print(acc)
+
+
+# In[20]:
+
+
+cm = confusion_matrix(Y_test, pred)
+# plt.figure(figsize=(20,10))
+sns.heatmap(cm, annot=True)
+
+
+# In[21]:
+
+
+print(classification_report(Y_test, pred))
+
+
+# In[22]:
+
+
+from sklearn.model_selection import GridSearchCV
+
+params = {'C':[0.1,1,10,100], 'kernel':['rbf'], 'gamma':[1,0.1,0.01,0.001]}
+
+grid = GridSearchCV(SVC(), params, refit=True, verbose = 4)
+
+
+# In[23]:
+
+
+grid.fit(X_train_scaled, Y_train)
+
+
+# In[24]:
+
+
+grid.best_params_
+
+
+# In[25]:
+
+
+grid_pred = grid.predict(X_test_scaled)
+acc = accuracy_score(Y_test, grid_pred)
+conf = confusion_matrix(Y_test, grid_pred)
+sns.heatmap(conf, annot=True)
+
+
+# In[26]:
+
+
+print(classification_report(Y_test, grid_pred))
+
+
+# In[27]:
+
+
+model1=SVC(C=1, gamma=1, kernel='rbf')
+model1.fit(X_train_scaled, Y_train)
+prediction=model1.predict(X_test_scaled)
+accuracy = accuracy_score(Y_test, prediction)
+print(accuracy)
+print(classification_report(Y_test, prediction))
 
 
 # In[ ]:
